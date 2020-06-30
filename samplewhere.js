b00647df7b85a0653ed2621fa5e69bc4fdc37c6f
srichethan <63234676+srichethan@users.noreply.github.com>
2020-06-30 14:00:09 +0530

    30-6-2020 14:00

--- samplewhere.js
+++
@@ -1,8 +0,0 @@
-myData = [
-[12.7409127,77.8252923, 'Hosur, Tamil Nadu, India'],
-[12.7409127,77.8252923, 'Hosur, Tamil Nadu, India'],
-[12.7409127,77.8252923, 'Hosur, Tamil Nadu, India'],
-[12.7409127,77.8252923, 'Hosur, Tamil Nadu, India'],
-[12.7409127,77.8252923, 'Hosur, Tamil Nadu, India'],
-[12.7409127,77.8252923, 'Hosur, Tamil Nadu, India']
-];
